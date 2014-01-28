d806733841f25b66082bc9714ab02e6d60cea13c
Likit Preeyanon <preeyano@msu.edu>
2014-01-28 00:05:26 -0500

    add disqus to conf.py

--- conf.py
+++ conf.py
@@ -93,6 +93,14 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+html_context = {
+   #"google_analytics_id" : 'UA-36028965-1',
+   "disqus_shortname" : 'ctubioinfocourse',
+   "github_base_account" : 'likit',
+   "github_project" : 'CTU-bioinfo-course',
+}
+
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
